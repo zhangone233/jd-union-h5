9e087c32271e8c8272b530b36cd8c3e4b1f0ec72
zhangone <zhangone233@126.com>
2023-08-11 01:35:58 +0800

    feat: 结构调整 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@pages': '/src/pages',
+      '@assets': '/src/assets',
+      '@components': '/src/components'
+    }
+  },
+  server: {
+    proxy: {
+      // '/api': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+    },
+  },
+});
